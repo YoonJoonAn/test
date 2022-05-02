9af6f5937815a838e30a6fee80d861e6119605fc
1nagi <nagi@DESKTOP-LKCFJG11111I>
2022-05-02 15:04:30 +0900

    test11111111

---
+++ nagi_test/src/main/java/nagi_test/test.java
@@ -0,0 +1,18 @@
+package nagi_test;
+
+import javax.swing.JFrame;
+
+public class test extends JFrame{
+	
+	public test() {
+		setTitle("swing frame 만들기");
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		test frame = new test();
+
+	}
+
+}
